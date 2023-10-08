dc121fbd9574e4720c969f7f7c859af2790beb87
Valeria Serci <balerty84@gmail.com>
2023-10-08 21:21:48 +0100

    create environment variables for  server host and port

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -4,6 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
 // declare a route with a response
 app.get('/', (req, res) => {
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
