b774ff60e77b024745f5f9d95fdd35353b76281e
Valeria Serci <balerty84@gmail.com>
2023-10-15 19:38:19 +0100

    #41-Create farmer Schema - Valeria

---
+++ backend/src/models/farmer.ts
@@ -0,0 +1,16 @@
+import { Schema, model, connect } from 'mongoose';
+
+// 1. Create an interface representing a document in MongoDB.
+interface IFarmer {
+  email: string;
+  password: string;
+}
+
+// 2. Create a Schema corresponding to the document interface.
+const farmerSchema = new Schema<IFarmer>({
+  email: { type: String, required: true },
+  password: { type: String, required: true }
+})
+
+// 3. Create a Model.
+const Farmer = model<IFarmer>('Farmer', farmerSchema);
