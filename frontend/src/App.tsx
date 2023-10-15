2a26095fb8f8b918f67465ad04621dc5d06deac9
Becky <bjentwistle@gmail.com>
2023-10-15 23:40:02 +0100

    added homepage component to test routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,23 @@
 // import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Nav/Navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HomePage from './components/Pages/HomePage';
 
 function App() {
   
   return (
     <>
-    <Navbar/>
-      
-      <h1>This is a test screen</h1>
+    <Router>
+      <div className='App'>
+        <Navbar/>
+        <div className='content'>
 
+          <HomePage />
+           
+        </div>
+      </div>
+    </Router>
     </>
   )
 }
---
+++ frontend/src/components/Pages/HomePage.tsx
@@ -0,0 +1,8 @@
+const HomePage : React.FC = () => {
+    return (
+        <h1>Test home page</h1>
+    )
+
+}
+
+export default HomePage;
