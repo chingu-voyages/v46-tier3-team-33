76ebb53df9b42d942d81d5ec4b4cbbf6539837a2
Valeria Serci <balerty84@gmail.com>
2023-10-16 21:52:31 +0100

    add check for valid email

--- backend/src/controllers/signupController.ts
+++ backend/src/controllers/signupController.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from "express"
 import { Farmer } from "../models/farmer";
 
+const emailRegexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
+
 const signup = (req: Request, res: Response) => {
+  if (!emailRegexp.test(req.body.email)){
+    return res.status(400).send('Invalid email')
+  }
   const farmer = new Farmer()
   farmer.email = req.body.email
   farmer.password = req.body.password
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,6 +18,5 @@ app.post('/signup', signup);
 // start the server
 app.listen(8081, () => {
   console.log(`server running : http://localhost:8081`);
-  console.log(db);
 });
 
