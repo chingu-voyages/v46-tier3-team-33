9c552e7a6ce378a8856c4cce1ea5a82781ab0e91
Jena <jenagansivakumar@gmail.com>
2023-10-16 18:00:28 +0200

    initial commit. installed email.js. created a form with: name, email address, subject

--- frontend/src/components/Pages/contact/Contact.tsx
+++ frontend/src/components/Pages/contact/Contact.tsx
@@ -1,5 +1,16 @@
 function Contact() {
-	return <h1>Contact</h1>;
+  return (
+    <div className="contact-page">
+      <form className="contact-form">
+        <label> Name </label>
+        <input placeholder="David Attenborough" />
+        <label> Email address </label>
+        <input placeholder="blueplanet@gmail.com" />
+        <label> Subject: </label>
+        <input placeholder="Saving the planet" />
+      </form>
+    </div>
+  );
 }
 
 export default Contact;
