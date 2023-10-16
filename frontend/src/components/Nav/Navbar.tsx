be996126c31bcfe7351d30129ce6917688cbe976
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-10-16 12:22:08 +0200

    45-feat: add contact to list

--- frontend/src/components/Nav/Navbar.tsx
+++ frontend/src/components/Nav/Navbar.tsx
@@ -1,36 +1,48 @@
-import React from 'react'
-import logo from '/src/assets/logo.svg'
-import './Navbar.css'
+import React from 'react';
+import logo from '/src/assets/logo.svg';
+import './Navbar.css';
 
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const Navbar : React.FC = () => {
-  return (
-    <>
-    <div className='navbar' >
-        <a href = '/'>
-            <img src={logo} className="logo" alt="logo" />
-            
-        </a>
-        <div className='links-container'>
-        <ul className = 'link links-container' >
-          <a>
-          <li><Link to ="/about">About</Link></li>
-          </a>
-          <a>
-          <li><Link to ="/help">Help</Link></li>
-          </a>
-          <a>
-          <li><Link to ="/signUp">Sign Up</Link></li>
-          </a>
-          <a>
-          <li><Link to ="/Login">Login</Link></li>
-          </a>
-        </ul>
-        </div>
-    </div>
-    </>
-  )
-}
+const Navbar: React.FC = () => {
+	return (
+		<>
+			<div className='navbar'>
+				<a href='/'>
+					<img src={logo} className='logo' alt='logo' />
+				</a>
+				<div className='links-container'>
+					<ul className='link links-container'>
+						<a>
+							<li>
+								<Link to='/about'>About</Link>
+							</li>
+						</a>
+						<a>
+							<li>
+								<Link to='/help'>Help</Link>
+							</li>
+						</a>
+						<a>
+							<li>
+								<Link to='/signUp'>Sign Up</Link>
+							</li>
+						</a>
+						<a>
+							<li>
+								<Link to='/Login'>Login</Link>
+							</li>
+						</a>
+						<a>
+							<li>
+								<Link to='/Contact'>Contact</Link>
+							</li>
+						</a>
+					</ul>
+				</div>
+			</div>
+		</>
+	);
+};
 
 export default Navbar;
