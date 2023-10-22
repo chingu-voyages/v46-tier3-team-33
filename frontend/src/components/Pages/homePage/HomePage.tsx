bf112cd4e07ebee4a6517387b9c9ec82e710869f
Becky <bjentwistle@gmail.com>
2023-10-22 15:11:20 +0100

    added search function and submit button - not working yet - Becky and Debbie

--- frontend/src/components/Pages/homePage/HomePage.tsx
+++ frontend/src/components/Pages/homePage/HomePage.tsx
@@ -10,10 +10,11 @@ function HomePage() {
 		<div className="container">
 			<div className="text_container">
 				<h1>Welcome to Vegilicious</h1>
-				<h3>The place to find fresh local and homegrown produce!</h3>	
+				<h3>The place to find fresh local and homegrown produce!</h3>
+				<Search></Search>
 			</div>
 			<img className="background" src="src/assets/inigo-de-la-maza-marketstall-unsplash.jpg" alt = "fresh vegetables and fruit" ></img>
-			<Search></Search>
+			
 		</div>
 	</>
 )}
--- frontend/src/components/Search/Search.tsx
+++ frontend/src/components/Search/Search.tsx
@@ -1,14 +1,36 @@
 import './search.css'
-//import React, {useState} from 'react'
+import {useState} from 'react'
 
 
 const searchBar = () => {
+    const [value, setValue] = useState('');
 
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log('search term:', value);
+
+    }
 
     return (
 
         <>
-        <p> Search for produce</p>
+        <form onSubmit={handleSubmit}>
+        <div className="search_bar">
+      <input 
+        type="text"
+        className="search_text"
+        placeholder="Search for veggies..."
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+        
+      />
+      <div className = 'search_submit' >
+			<button type="submit" >Search</button>
+		</div>
+    </div>
+    </form>
         </>
     )
 }
