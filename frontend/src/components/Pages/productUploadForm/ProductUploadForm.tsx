8c77fecd15cfe6ec05b2b12ef37ccf5e6ba0c702
Jena <jenagansivakumar@gmail.com>
2023-10-25 13:51:20 +0200

    added ids and labels to all the elements

--- frontend/src/components/Pages/productUploadForm/ProductUploadForm.tsx
+++ frontend/src/components/Pages/productUploadForm/ProductUploadForm.tsx
@@ -32,15 +32,14 @@ export default function ProductUploadForm() {
     const { name, value } = e.target;
     setFormValues((prev) => ({ ...prev, [name]: value }));
   };
+
   const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     const { name, value } = e.target;
-
     let newValue = value;
     if (name === "expiryDate") {
       newValue = newValue.replace(/[^0-9]/g, "");
-
       if (newValue.length > 2) {
         newValue = `${newValue.slice(0, 2)}/${newValue.slice(2)}`;
       }
@@ -50,18 +49,23 @@ export default function ProductUploadForm() {
     }
     setFormValues((prev) => ({ ...prev, [name]: newValue }));
   };
+
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
       setFormValues((prev) => ({ ...prev, picture: e.target.files[0] }));
     }
   };
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     console.log(formValues);
   };
+
   return (
     <form className="upload-form" onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
       <input
+        id="name"
         className="upload-form-input"
         type="text"
         name="name"
@@ -69,20 +73,29 @@ export default function ProductUploadForm() {
         onChange={handleInputChange}
         value={formValues.name}
       />
+
+      <label htmlFor="picture">Upload Product Image</label>
       <input
+        id="picture"
         className="upload-form-input"
         type="file"
         name="picture"
         onChange={handleImageChange}
       />
+
+      <label htmlFor="description">Description of Product</label>
       <textarea
+        id="description"
         name="description"
         placeholder="Describe your produce"
         onChange={handleInputChange}
         value={formValues.description}
         className="upload-form-textarea"
       />
+
+      <label htmlFor="postcode">Postcode</label>
       <input
+        id="postcode"
         className="upload-form-input"
         type="text"
         name="postcode"
@@ -90,15 +103,21 @@ export default function ProductUploadForm() {
         onChange={handleInputChange}
         value={formValues.postcode}
       />
+
+      <label htmlFor="price">Price in £</label>
       <input
+        id="price"
         className="upload-form-input"
         type="number"
         name="price"
-        placeholder="Price in £"
+        placeholder="9"
         onChange={handleInputChange}
         value={formValues.price}
       />
+
+      <label htmlFor="quantityAvailable">Available Quantity</label>
       <input
+        id="quantityAvailable"
         className="upload-form-input"
         type="number"
         name="quantityAvailable"
@@ -106,24 +125,27 @@ export default function ProductUploadForm() {
         onChange={handleInputChange}
         value={formValues.quantityAvailable}
       />
-      <label htmlFor="unitOfMeasure"> Unit of Measure </label>
+
+      <label htmlFor="unitOfMeasure">Units</label>
       <select
         className="upload-form-select"
         id="unitOfMeasure"
         name="unitOfMeasure"
         onChange={handleSelectChange}
       >
-        <option value="g"> Gram (g)</option>
-        <option value="kg"> Kilogram (kg)</option>
-        <option value="oz"> Ounce (oz)</option>
-        <option value="lb"> Pound (lb)</option>
-        <option value="ml"> Millilitre (ml)</option>
-        <option value="l"> Litre (l)</option>
-        <option value="items"> Individual Items</option>
+        <option value="g">Gram (g)</option>
+        <option value="kg">Kilogram (kg)</option>
+        <option value="oz">Ounce (oz)</option>
+        <option value="lb">Pound (lb)</option>
+        <option value="ml">Millilitre (ml)</option>
+        <option value="l">Litre (l)</option>
+        <option value="items">Individual Items</option>
       </select>
 
+      <label htmlFor="expiryDate">Expiry Date</label>
       <input
-        type="string"
+        id="expiryDate"
+        type="text"
         name="expiryDate"
         placeholder="DD/MM/YYYY"
         onChange={handleInputChange}
@@ -131,24 +153,19 @@ export default function ProductUploadForm() {
         maxLength={10}
         className="upload-form-input"
       />
+
+      <label htmlFor="availabilityTime">Pick Up Time</label>
       <input
-        type="number"
-        name="quantityAvailable"
-        placeholder="Quantity Available"
-        onChange={handleInputChange}
-        value={formValues.quantityAvailable}
-        className="upload-form-input"
-      />
-      <input
-        type="string"
+        id="availabilityTime"
+        type="text"
         name="availabilityTime"
         placeholder="When to pick it up?"
         onChange={handleInputChange}
         value={formValues.availabilityTime}
         className="upload-form-input"
       />
+
       <button className="upload-form-button" type="submit">
-        {" "}
         Submit
       </button>
     </form>
