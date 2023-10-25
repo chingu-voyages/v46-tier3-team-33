279aa230d031e151429fa26996130f0c8767fa68
Valeria Serci <balerty84@gmail.com>
2023-10-25 23:27:24 +0100

    fix destination folder for uploads

--- backend/src/multerSetup.ts
+++ backend/src/multerSetup.ts
@@ -6,23 +6,23 @@ import fs from "fs";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const pathToReactPublic = path.join(__dirname, '..', 'frontend', 'public', 'uploads')
-    if (!fs.existsSync(pathToReactPublic)) { 
+    const pathToReactPublic = path.join(__dirname, '..', '..','frontend', 'public', 'uploads')
+    if (!fs.existsSync(pathToReactPublic)) {
       fs.mkdirSync(pathToReactPublic, { recursive: true }); // creates folders if don't exist
     }
-    cb(null, pathToReactPublic); //img filepath
+    cb(null, pathToReactPublic); // img filepath
   },
 
   filename: (req, file, cb) => {
-    cb(null, `${v4()}-${Date.now()}${path.extname(file.originalname)}`); //img filename 
+    cb(null, `${v4()}-${Date.now()}${path.extname(file.originalname)}`); // img filename
   }
 });
 
 const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
   const allowedFileTypes = ['image/jpeg', 'image/jpg', 'image/png'];
-  
+
   if (allowedFileTypes.includes(file.mimetype)) { cb(null, true); }
-  else { cb(null, false); } 
+  else { cb(null, false); }
 };
 
 const upload = multer({ storage, fileFilter });
