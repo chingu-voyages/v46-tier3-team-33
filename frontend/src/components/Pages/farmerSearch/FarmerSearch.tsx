ec30d0d219b929866002ed57a768cb8d54f7978e
Jena <jenagansivakumar@gmail.com>
2023-10-26 18:54:33 +0200

    finished UI and event handlers for the form. Set up code to update the state as well

--- frontend/src/components/Pages/farmerSearch/FarmerSearch.tsx
+++ frontend/src/components/Pages/farmerSearch/FarmerSearch.tsx
@@ -1,14 +1,35 @@
-import { Farmer } from "./types";
+import { useState } from "react";
 
 interface Props {
   onSearch: (query: string) => void;
 }
 
-export default function FarmerSearch() {
+export default function FarmerSearch({ onSearch }: Props) {
+  const [value, setValue] = useState("");
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setValue(e.target.value);
+  }
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    onSearch(value);
+  }
   return (
     <>
-      <form>
-        <input />
+      <form onSubmit={handleSubmit}>
+        <input
+          required
+          type="text"
+          className="search_text"
+          placeholder="Search for farmers..."
+          value={value}
+          onChange={handleChange}
+        />
+        <div className="search_submit">
+          <button className="button" type="submit">
+            {" "}
+            Search Farmer
+          </button>
+        </div>
       </form>
     </>
   );
