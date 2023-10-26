c4acadf19548831fec90a67188510451a80ca3f7
Jena <jenagansivakumar@gmail.com>
2023-10-26 17:10:33 +0200

    starting the FarmerSearch component

---
+++ frontend/src/components/Pages/farmerSearch/FarmerSearch.tsx
@@ -0,0 +1,15 @@
+import { Farmer } from "./types";
+
+interface Props {
+  onSearch: (query: string) => void;
+}
+
+export default function FarmerSearch() {
+  return (
+    <>
+      <form>
+        <input />
+      </form>
+    </>
+  );
+}
