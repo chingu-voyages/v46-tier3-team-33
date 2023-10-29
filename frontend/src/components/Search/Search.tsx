57f81ad56c4f468392820ccbfdc962a52e4ecc80
Becky <bjentwistle@gmail.com>
2023-10-29 14:23:14 +0000

    managed to display filtered results on searchpage - Becky

--- frontend/src/components/Search/Search.tsx
+++ frontend/src/components/Search/Search.tsx
@@ -1,14 +1,54 @@
 import './search.css'
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 
 
 const searchBar = () => {
     const [value, setValue] = useState('');
+    const [data, setData] = useState([]);
+    const [filteredData, setFilteredData] = useState<any[]>([]); // Use 'any' as a temporary type
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    // useEffect(() => {
+    //     //fetch('http://localhost:8081/product?_limit=10') //need to send token to backend for validation
+    //     fetch('https://jsonplaceholder.typicode.com/posts?_limit=10') //using placeholder data for now
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             console.log(data);
+    //             setData(data);
+    //        })
+    //        .catch((err) => {
+    //             console.log(err.message);
+    //             //GET http://localhost:8081/product?_limit=10 401 (Unauthorized) get this error if token not sent
+    //        });
+    //  }, []);
 
-        console.log('search term:', value);
+     const fetchData = async () => {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10');
+            if (response.ok) {
+                const jsonData = await response.json();
+                setData(jsonData);
+
+                // Apply filtering here
+                const filtered = jsonData.filter((item: any) =>
+                    item.title.toLowerCase().includes(value.toLowerCase())
+                );
+                setFilteredData(filtered);
+                console.log("Filtered results:",filtered)
+            } else {
+                console.error('API request failed with status:', response.status);
+            }
+        } catch (error: any) {
+            console.error('API request error:', error.message);
+        }
+    };
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();    
+        console.log('search term:', value);    
+
+         // Call the fetchData function to make the API request and apply filtering
+         fetchData();
+        
     }
 
     return (
@@ -31,6 +71,18 @@ const searchBar = () => {
                 </div>
             </div>
         </form>
+        {/* Conditionally render the filtered data */}
+        {filteredData.length > 0 ? (
+                <div className="filtered_data">
+                    <ul>
+                        {filteredData.map((item: any) => (
+                            <li key={item.id}>{item.title}</li>
+                        ))}
+                    </ul>
+                </div>
+            ) : (
+                <div>No matching results found</div>
+            )}
         </>
     )
 }
