844c1530ff80614dcc592b7625652610390547f5
Valeria Serci <balerty84@gmail.com>
2023-11-01 22:56:42 +0000

    add preSignup component

---
+++ frontend/src/components/Pages/preSignup/PreSignup.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./preSignup.css"
+
+const PreSignup: React.FC = () => {
+
+  return (
+    <div className="preSignup-god-container">
+      <div id='preSignup-container'>
+        <h1> Signup as a Farmer </h1>
+          
+        <p className='prompt-login-text'>Have an account?  <a href="/login" className='prompt-login-link'>Login</a> now</p>
+      </div>
+				
+
+    </div> 
+  )
+};
+
+export default PreSignup;
