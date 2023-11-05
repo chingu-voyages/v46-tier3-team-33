0b568a14c8165e3f2d8f46106f193f0972cb2405
Becky <bjentwistle@gmail.com>
2023-11-05 13:04:02 +0000

    added all product details to results card

--- frontend/src/components/Search/searchResultCard.tsx
+++ frontend/src/components/Search/searchResultCard.tsx
@@ -2,10 +2,18 @@ import './search.css'
 
 // Define the Item type
 type Item = {
-    userId: number;
-    id: number;
-    title: string;
-    body: string;
+    _id:Object;
+    userId: string;
+    name: string;
+    picture: string;
+    description:string;
+    postcode:string;
+    price: number;
+    quantityOfUnit: number;
+    unitOfMeasure: string;
+    expiryDate: string;
+    quantityAvailable: number;
+    availabilityTime: string;
   };
   
 // Use the Item type to type the item parameter
@@ -14,19 +22,31 @@ const SearchResultCard: React.FC<{ item: Item }> = ({ item }) => {
   return (
     <div className="search-result-card">
       <div className="image-container">
-        {/* Placeholder for image */}
-        {/* we can replace this with an actual image */}
         <img
-          src="https://via.placeholder.com/100"
-          alt="Result Image"
+          src={item.picture}
+          alt={item.description}
         />
       </div>
       <div className="card-content">
-        <strong>Title:</strong> {item.title}
+        <strong>Name:</strong> {item.name}
         <br />
         <strong>User ID:</strong> {item.userId}
         <br />
-        <strong>Body:</strong> {item.body}
+        <strong>Description:</strong> {item.description}
+        <br />
+        <strong>Postcode:</strong> {item.postcode}
+        <br />
+        <strong>Price per unit:</strong> {item.price}
+        <br />
+        <strong>Quantity of unit:</strong> {item.quantityOfUnit}
+        <br />
+        <strong>Unit of measure:</strong> {item.unitOfMeasure}
+        <br />
+        <strong>Expiry date:</strong> {item.expiryDate}
+        <br />
+        <strong>Quantity available:</strong> {item.quantityAvailable}
+        <br />
+        <strong>Availability time:</strong> {item.availabilityTime}
       </div>
     </div>
   );
