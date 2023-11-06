f108d9c927c5ec731f6c5fe1a056ede3b368a020
Deborah Thompson <deb246@btinternet.com>
2023-11-06 11:19:54 +0000

    about page content created

--- frontend/src/components/Pages/about/About.tsx
+++ frontend/src/components/Pages/about/About.tsx
@@ -1,5 +1,24 @@
+
+
+import "./about.css"
+
 function About() {
-	return <h1>About</h1>;
+  return (
+    <div className="container">
+      <h1>About Us</h1>
+      <p>We are a team of developers who have put together this app to connect</p>
+	  <p>local people with local producers of fruit and vegetables.</p>
+	  <p>We all have different experiences in coding, which we  bring to this app</p>
+	  <p>We are all part of the <a href="https://www.chingu.io/" target="_blank">Chingu voyage 46 experience</a></p>
+	  <p>and have worked hard to get this app completed in the 6 week timeframe we have.</p>
+
+	  <p>So if you are looking for fresh fruit and veggies or if you are a</p>
+	  <p>producer with fruit and veggies to sell this app provides you with</p>
+	  <p>a way to do that.</p>
+	  
+    </div>
+  );
 }
 
-export default About;
+export default About
+
