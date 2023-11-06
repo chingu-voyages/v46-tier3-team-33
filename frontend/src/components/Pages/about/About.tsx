28e5af94b936d5c5e19aeb3d937e9bdd4d569c93
Deborah Thompson <deb246@btinternet.com>
2023-11-06 11:24:59 +0000

    about page text created

--- frontend/src/components/Pages/about/About.tsx
+++ frontend/src/components/Pages/about/About.tsx
@@ -10,7 +10,7 @@ function About() {
 	  <p>local people with local producers of fruit and vegetables.</p>
 	  <p>We all have different experiences in coding, which we  bring to this app.</p>
 	  <p>We are all part of the <a href="https://www.chingu.io/" target="_blank">Chingu voyage 46 experience</a></p>
-	  <p>and have worked hard to get this app completed in the 6 week timeframe we have.</p>
+	  <p>and we have worked hard to get this app completed in the 6 week timeframe we have.</p>
 
 	  <p>So if you are looking for fresh fruit and veggies or if you are a</p>
 	  <p>producer with fruit and veggies to sell this app provides you with</p>
