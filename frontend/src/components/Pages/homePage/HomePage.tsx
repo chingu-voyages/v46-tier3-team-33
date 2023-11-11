6eea4b2205f5751c553a4c4b48f5077f3d514c62
Becky <bjentwistle@gmail.com>
2023-11-11 16:21:45 +0000

    adjusted some css in search results

--- frontend/src/components/Pages/homePage/HomePage.tsx
+++ frontend/src/components/Pages/homePage/HomePage.tsx
@@ -5,15 +5,9 @@ function HomePage() {
   return (
     <>
       <div className="container">
-        {/* <img
-          className="background_image"
-          src="src/assets/vegelicious_hero.png"
-          alt="fresh vegetables and fruit"
-        ></img> */}
 
         <div className="text-container">
-          {/* <h1>Welcome to Vegilicious</h1>
-          <h3>The place to find fresh local and homegrown produce!</h3> */}
+          
           <Search></Search>
         </div>
       </div>
