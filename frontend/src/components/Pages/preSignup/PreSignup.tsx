b0db03e15b969a808d1fedfeb26179913009a3da
Robi <robi.maliqi@hotmail.co.uk>
2023-11-19 13:23:31 +0000

    Fixed to Link to render

--- frontend/src/components/Pages/preSignup/PreSignup.tsx
+++ frontend/src/components/Pages/preSignup/PreSignup.tsx
@@ -1,17 +1,21 @@
 import React from "react";
-import "./preSignup.css"
+import "./preSignup.css";
+import { Link } from "react-router-dom";
 
 const PreSignup: React.FC = () => {
-
   return (
     <div className="preSignup-god-container">
-      <div id='preSignup-container'>
+      <div id="preSignup-container">
         <h1>Sign up as</h1>
-        <a className="button" href="/SignUp/farmer">Farmer</a>
-        <a className="button" href="/SignUp/customer">Customer</a>
+        <Link to="/SignUp/farmer" className="button">
+          Farmer
+        </Link>
+        <Link to="/SignUp/customer" className="button">
+          Customer
+        </Link>
       </div>
-    </div> 
-  )
+    </div>
+  );
 };
 
 export default PreSignup;
